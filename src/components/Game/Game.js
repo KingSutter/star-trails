a9f03401f6122b0ca8d0f94c64f0a0b86cdd56c1
Will Sutter <kingsutter12@gmail.com>
2019-11-18 19:20:15 -0600

    added remaining inputs and remaining credits is dynamically caculated

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import './Game.css'
+import { tsMethodSignature } from '@babel/types';
 
 class Game extends Component{
     state= {
@@ -15,7 +16,8 @@ class Game extends Component{
         resource3: 0,
         resource4: 0,
         resource5: 0,
-        money: 800
+        money: 800,
+        available: 800,
     }
     // sends all data to server and associates that save data with the current user
     createSaveAndStart = () => {
@@ -23,6 +25,49 @@ class Game extends Component{
             console.log("yes");
         }
     }
+    handleFoodChange = (e) => {
+        this.setState({
+            food: Number(e.target.value),
+            available: this.state.money - (Number(e.target.value)*.2) - this.state.resource1*10 - this.state.resource2*2 -
+            this.state.resource3*10 - this.state.resource4*10 - this.state.resource5*10
+        })
+    }
+    handleClothesChange = (e) => {
+        this.setState({
+            resource1: Number(e.target.value),
+            available: this.state.money - this.state.food*.2 - e.target.value*10 - this.state.resource2*2 -
+            this.state.resource3*10 - this.state.resource4*10 - this.state.resource5*10
+        })
+    }
+    handleAmmoChange = (e) => {
+        this.setState({
+            resource2: Number(e.target.value),
+            available: this.state.money - this.state.food*.2 - this.state.resource1*10 - e.target.value*2 -
+            this.state.resource3*10 - this.state.resource4*10 - this.state.resource5*10
+        })
+    }
+    handleSpare1Change = (e) => {
+        this.setState({
+            resource3: Number(e.target.value),
+            available: this.state.money - this.state.food*.2 - this.state.resource1*10 - this.state.resource2*2 -
+            e.target.value*10 - this.state.resource4*10 - this.state.resource5*10
+        })
+    }
+    handleSpare2Change = (e) => {
+        this.setState({
+            resource4: Number(e.target.value),
+            available: this.state.money - this.state.food*.2 - this.state.resource1*10 - this.state.resource2*2 -
+            this.state.resource3*10 - e.target.value*10 - this.state.resource5*10
+        })
+    }
+    handleSpare3Change = (e) => {
+        this.setState({
+            resource5: Number(e.target.value),
+            available: this.state.money - this.state.food*.2 - this.state.resource1*10 - this.state.resource2*2 -
+            this.state.resource3*10 - this.state.resource4*10 - e.target.value*10
+        })
+    }
+
     render(){
         return(
             <form onSubmit={this.createSaveAndStart}>
@@ -36,8 +81,30 @@ class Game extends Component{
                     <li>Tactical: <input placeholder="name" required /></li>
                 </ul>
                 <h2>Food</h2>
-                    <ul><input placeholder="pounds" required /> Cost: ⌬20 per pound</ul>
-                <p id="credits">Credits: ⌬{this.state.money}</p>
+                <ul><input onChange={this.handleFoodChange} placeholder="pounds" type="number" min="0" required /> Cost: ⌬0.20 per pound</ul>
+                <h2>Clothes</h2>
+                <ul><input onChange={this.handleClothesChange} placeholder="sets of clothing" type="number" min="0" required /> Cost: ⌬10 per set</ul>
+                <h2>Ammo</h2>
+                <ul><input onChange={this.handleAmmoChange} placeholder="boxes of ammo" type="number" min="0" required /> Cost: ⌬2 per box</ul>
+                <h2>Spare Parts</h2>
+                <ul>
+                    <li><input onChange={this.handleSpare1Change} placeholder="number" type="number" min="0" required /> Cost: ⌬10 per </li>
+                    <li><input onChange={this.handleSpare2Change} placeholder="number" type="number" min="0" required /> Cost: ⌬10 per </li>
+                    <li><input onChange={this.handleSpare3Change} placeholder="number" type="number" min="0" required /> Cost: ⌬10 per </li>
+                </ul>
+                <div id="bill">
+                    <h2>Bill:</h2>
+                    <ul>
+                        <li>Food: ⌬{this.state.food * .2}</li>
+                        <li>Clothes: ⌬{this.state.resource1 * 10}</li>
+                        <li>Ammo: ⌬{this.state.resource2 * 2}</li>
+                        <li>Spare Part 1: ⌬{this.state.resource3 * 10}</li>
+                        <li>Spare Part 2: ⌬{this.state.resource4 * 10}</li>
+                        <li>Spare Part 3: ⌬{this.state.resource5 * 10}</li>
+                    </ul>
+                </div>
+                {/* <div id="totalCredits">Total Credits: ⌬{this.state.money}</div> */}
+                <div id="availableCredits">Available Credits: ⌬{this.state.available}</div>
                 <div id="startButton">
                     <button  type="submit">Start your journey</button>
                 </div>
