9df16afe22cb71705bd5863f218c74bb1ab0aa38
Will Sutter <kingsutter12@gmail.com>
2019-11-26 15:58:22 -0600

    helper function created to detect if an animal was hit

--- src/components/Game/HuntingGame/HuntingGame.js
+++ src/components/Game/HuntingGame/HuntingGame.js
@@ -6,6 +6,7 @@ class HuntingGame extends Component {
 
     secondsCounter = '';
     seconds= 0;
+    foodGathered = 0;
 
     state = {
         grid: [
@@ -21,7 +22,7 @@ class HuntingGame extends Component {
             ["", "", "", "", "", "", "", "", "", ""],
         ],
         hunter: {
-            image: "🧍",
+            image: "⇢",
             position: [5,5],
             direction: "right",
         },
@@ -29,27 +30,31 @@ class HuntingGame extends Component {
             {
                 image: "👾",
                 position: [2,5],
-                behavior: "down"
+                behavior: "down",
+                isAlive: true,
             },
             {
                 image: "👾",
                 position: [4,0],
-                behavior: "up"
+                behavior: "up",
+                isAlive: true,
             },
             {
                 image: "👾",
                 position: [6,9],
-                behavior: "left"
+                behavior: "left",
+                isAlive: true,
             },
             {
                 image: "👾",
                 position: [1,2],
-                behavior: "right"
+                behavior: "right",
+                isAlive: true,
             },
         ],
     }
     componentDidMount(){
-        this.secondsCounter = setInterval(this.handleSeconds, 300); // every .25 seconds
+        this.secondsCounter = setInterval(this.handleSeconds, 200); // every .2 seconds
         document.addEventListener('keydown', this.handleKeyPress);
         this.mapObjectsToGrid();
     }
@@ -61,7 +66,7 @@ class HuntingGame extends Component {
         // console.log(this.seconds);
         this.updateGrid();
     }
-handleKeyPress = (e) => {
+    handleKeyPress = (e) => {
         // event listeners for key presses
         if (e.code === "ArrowUp") {
             this.setState({
@@ -145,7 +150,9 @@ handleKeyPress = (e) => {
         // console.log(e.code);
         
     }
-    mapObjectsToGrid = (laserCoords = null, hunterPosition = [this.state.hunter.position[0], this.state.hunter.position[1]]) => {
+
+    // updates grid and draws a laser if inputted
+    mapObjectsToGrid = (hunterCoords = null, hunterPosition = [this.state.hunter.position[0], this.state.hunter.position[1]]) => {
         let newGrid = [
             ["", "", "", "", "", "", "", "", "", ""],
             ["", "", "", "", "", "", "", "", "", ""],
@@ -166,36 +173,40 @@ handleKeyPress = (e) => {
             newGrid[animal.position[0]][animal.position[1]] = animal.image;
         });
         // draw laser if there are coords inputted
-        if (laserCoords !== null){
+        if (hunterCoords !== null){
             console.log("laser fired!");
-            const columnLine = laserCoords[1];
-            const rowLine = laserCoords[0];
+            const columnLine = hunterCoords[1];
+            const rowLine = hunterCoords[0];
             switch (this.state.hunter.direction){
                 case "up":
                     console.log("up");
-                    for (let row = laserCoords[0]-1; row >= 0; row--) {
+                    for (let row = hunterCoords[0]-1; row >= 0; row--) {
                         newGrid[row][columnLine] = "🟥";
+                        this.checkForHit([row,columnLine]);
                         console.log(row,columnLine);
                     }
                     break;
                 case "down":
                     console.log("down");
-                    for (let row = laserCoords[0]+1; row < newGrid.length; row++) {
+                    for (let row = hunterCoords[0]+1; row < newGrid.length; row++) {
                         newGrid[row][columnLine] = "🟥";
+                        this.checkForHit([row,columnLine]);
                         console.log(row,columnLine);
                     }
                     break;
                 case "left":
                     console.log("left");
-                    for (let column = laserCoords[1]-1; column >= 0; column--) {
+                    for (let column = hunterCoords[1]-1; column >= 0; column--) {
                         newGrid[rowLine][column] = "🟥";
+                        this.checkForHit([rowLine,column]);
                         console.log(rowLine,column);
                     }
                     break;
                 case "right":
                     console.log("right");
-                    for (let column = laserCoords[1]+1; column < newGrid.length; column++) {
+                    for (let column = hunterCoords[1]+1; column < newGrid.length; column++) {
                         newGrid[rowLine][column] = "🟥";
+                        this.checkForHit([rowLine,column]);
                         console.log(rowLine,column);
                     }
                     break;
@@ -204,13 +215,14 @@ handleKeyPress = (e) => {
         
         this.setState({grid: newGrid})
     }
-    // moves animals based on animal behavior settings
+
+    // moves animals based on animal's behavior settings
     updateGrid = () => {
         // copy of animals in state
         let updatedAnimals = this.state.animals.slice();        
         // move and map animals
         updatedAnimals.forEach((animal, index) => {
-            if(this.props.randomInt(0,1)){
+            if(animal.isAlive && this.props.randomInt(0,1)){
                 if (animal.behavior === "up") {
                     if (animal.position[0] === 0) updatedAnimals.splice(index, 1);
                     else animal.position[0] -= 1;
@@ -231,6 +243,16 @@ handleKeyPress = (e) => {
         });
         this.setState({animals: updatedAnimals})
         this.mapObjectsToGrid();
+    }
+
+    // checks if any of the coordinates match an animal's coordinates
+    checkForHit = (laserCoords) => {
+        this.state.animals.forEach((animal)=> {
+            // console.log("laser", laserCoords, "animal", animal.position)
+            if (animal.isAlive && animal.position[0] === laserCoords[0] && animal.position[1] === laserCoords[1]){
+                console.log("animal hit!", animal)
+            }
+        });
     }
 
     render(){
