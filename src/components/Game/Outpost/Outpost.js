58c16877b68d008f35a8fe0a85e6a1ed072daba3
Will Sutter <kingsutter12@gmail.com>
2019-12-02 00:21:32 -0600

    added input validation for entering values to the merchant

--- src/components/Game/Outpost/Outpost.js
+++ src/components/Game/Outpost/Outpost.js
@@ -17,7 +17,9 @@ class Outpost extends Component {
       availableConstrictors: 0,
       availableInjectors: 0,
       total: 0,
+      errorMessage: 'placeholder',
     }
+
     componentDidMount(){
       this.setState({
         availableFood: this.props.saveData.food,
@@ -30,46 +32,83 @@ class Outpost extends Component {
       })
     }
 
+    // checks if the change can be made then increases said item
     incrementItem = (item, price, quantity=1) => {
-      this.setState({
-        [item]: this.state[item] + quantity,
-        total: this.state.total + (price * quantity),
-      })
+      // If there is a problem, tell the user about it
+      if(this.state.availableCredits - (price * quantity) < 0 || this.state.availableCredits < 0){
+        document.getElementById("warning").style.color = "white";
+        document.getElementById("warning").style.backgroundColor = "red";
+        this.setState({errorMessage: "You cannot go into debt! Please fix that first."})
+        return 0;
+      } else { // otherwise, don't...
+        document.getElementById("warning").style.color = "transparent";
+        document.getElementById("warning").style.backgroundColor = "transparent";
+        this.setState({
+          [item]: this.state[item] + quantity,
+          total: this.state.total + (price * quantity),
+          availableCredits: this.state.availableCredits - (price * quantity),
+        })
+      }
     }
+
+    // checks if the change can be made then decreases said item
     decrementItem = (item, price, quantity=1) => {
-      this.setState({
-        [item]: this.state[item] - quantity,
-        total: this.state.total - (price * quantity),
-      })
+      // Checks using current available(whatever item was input) against decrement amount 
+      const availableItem = "available" + (item.charAt(0).toUpperCase() + item.slice(1))
+      // If there is a problem, tell the user about it
+      if((this.state[availableItem] + this.state[item]) - quantity < 0){
+        document.getElementById("warning").style.color = "white";
+        document.getElementById("warning").style.backgroundColor = "red";
+        this.setState({errorMessage: "You can't sell what you don't have!"})
+        return 0;
+      } else { // otherwise, don't...
+        document.getElementById("warning").style.color = "transparent";
+        document.getElementById("warning").style.backgroundColor = "transparent";
+        this.setState({
+          [item]: this.state[item] - quantity,
+          total: this.state.total - (price * quantity),
+          availableCredits: this.state.availableCredits + (price * quantity),
+        })
+      }
     }
+
+    // resets changes
     resetItem = (item, price) => {
+      document.getElementById("warning").style.color = "transparent";
+      document.getElementById("warning").style.backgroundColor = "transparent";
       this.setState({
         total: this.state.total - (this.state[item] * price),
+        availableCredits: this.state.availableCredits + (this.state[item] * price),
         [item]: 0,
       })
     }
+
+
     render(){
         return(
             <div id="outpostView">
                 <table>
                     <thead>
-                        <tr>
-                            <td>Item</td>
-                            <td>Price</td>
-                            <td>Buy/Sell</td>
-                            <td>Available</td>
-                            <td>Difference</td>
-                            <td>Total</td>
-                        </tr>
+                      <tr id="warning">
+                        <td colSpan="6">{this.state.errorMessage}</td>
+                      </tr>
+                      <tr className="header">
+                          <td>Item</td>
+                          <td>Price</td>
+                          <td>Buy/Sell</td>
+                          <td>Current</td>
+                          <td>Difference</td>
+                          <td>Total Cost</td>
+                      </tr>
                     </thead>
                     <tbody>
                         <tr>
                           <td>Food</td>
                           <td>⌬1</td>
-                          <td><button onClick={()=>this.incrementItem("food",1,10)} className="outpostButton">+</button><button onClick={()=>this.decrementItem("food",1)} className="outpostButton">-</button><button onClick={()=>this.resetItem("food",1)}>C</button></td>
+                          <td><button onClick={()=>this.incrementItem("food",1,10)} className="outpostButton">+</button><button onClick={()=>this.decrementItem("food",1,10)} className="outpostButton">-</button><button onClick={()=>this.resetItem("food",1)}>C</button></td>
                           <td>{this.state.availableFood + this.state.food}</td>
                           <td>{this.state.food}</td>
-                          <td>{this.state.food * 1}</td>
+                          <td>⌬{this.state.food * 1}</td>
                         </tr>
                         <tr>
                           <td>Batteries</td>
@@ -77,54 +116,63 @@ class Outpost extends Component {
                           <td><button onClick={()=>this.incrementItem("batteries", 2)} className="outpostButton">+</button><button onClick={()=>this.decrementItem("batteries",2)} className="outpostButton">-</button><button onClick={()=>this.resetItem("batteries",2)}>C</button></td>
                           <td>{this.state.availableBatteries + this.state.batteries}</td>
                           <td>{this.state.batteries}</td>
-                          <td>{this.state.batteries * 2}</td>
+                          <td>⌬{this.state.batteries * 2}</td>
                         </tr>
                         <tr>
                           <td>Warp Coils</td>
                           <td>⌬40</td>
-                          <td><button onClick={()=>this.incrementItem("coils", 40)} className="outpostButton">+</button><button onClick={()=>this.decrementItem("coils",40)} className="outpostButton">-</button><button onClick={()=>this.resetItem("coils",40)}>C</button></td>
+                          <td><button onClick={()=>this.incrementItem("coils", 40)}>+</button><button onClick={()=>this.decrementItem("coils",40)}>-</button><button onClick={()=>this.resetItem("coils",40)}>C</button></td>
                           <td>{this.state.availableCoils + this.state.coils}</td>
                           <td>{this.state.coils}</td>
-                          <td>{this.state.coils * 40}</td>
+                          <td>⌬{this.state.coils * 40}</td>
                         </tr>
                         <tr>
                           <td>Antimatter Flow Regulators</td>
                           <td>⌬10</td>
-                          <td><button onClick={()=>this.incrementItem("regulators", 10)} className="outpostButton">+</button><button onClick={()=>this.decrementItem("regulators",10)} className="outpostButton">-</button><button onClick={()=>this.resetItem("regulators",10)}>C</button></td>
+                          <td><button onClick={()=>this.incrementItem("regulators", 10)}>+</button><button onClick={()=>this.decrementItem("regulators",10)}>-</button><button onClick={()=>this.resetItem("regulators",10)}>C</button></td>
                           <td>{this.state.availableRegulators + this.state.regulators}</td>
                           <td>{this.state.regulators}</td>
-                          <td>{this.state.regulators * 10}</td>
+                          <td>⌬{this.state.regulators * 10}</td>
                         </tr>
                         <tr>
                           <td>Magnetic Constrictors</td>
                           <td>⌬10</td>
-                          <td><button onClick={()=>this.incrementItem("constrictors",10)} className="outpostButton">+</button><button onClick={()=>this.decrementItem("constrictors",10)} className="outpostButton">-</button><button onClick={()=>this.resetItem("constrictors",10)}>C</button></td>
+                          <td><button onClick={()=>this.incrementItem("constrictors",10)}>+</button><button onClick={()=>this.decrementItem("constrictors",10)}>-</button><button onClick={()=>this.resetItem("constrictors",10)}>C</button></td>
                           <td>{this.state.availableConstrictors + this.state.constrictors}</td>
                           <td>{this.state.constrictors}</td>
-                          <td>{this.state.constrictors * 10}</td>
+                          <td>⌬{this.state.constrictors * 10}</td>
                         </tr>
                         <tr>
                           <td>Plasma Injectors</td>
                           <td>⌬10</td>
-                          <td><button onClick={()=>this.incrementItem("injectors",10)} className="outpostButton">+</button><button onClick={()=>this.decrementItem("injectors",10)} className="outpostButton">-</button><button onClick={()=>this.resetItem("injectors",10)}>C</button></td>
+                          <td><button onClick={()=>this.incrementItem("injectors",10)}>+</button><button onClick={()=>this.decrementItem("injectors",10)}>-</button><button onClick={()=>this.resetItem("injectors",10)}>C</button></td>
                           <td>{this.state.availableInjectors + this.state.injectors}</td>
                           <td>{this.state.injectors}</td>
-                          <td>{this.state.injectors * 10}</td>
+                          <td>⌬{this.state.injectors * 10}</td>
                         </tr>
                     </tbody>
                     <tfoot>
+                        <tr className="footer">
+                          <td>Spending Total</td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td>⌬{this.state.total}</td>
+                        </tr>
+                        <tr className="footer">
+                          <td>Available Credits</td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td>⌬{this.state.availableCredits}</td>
+                        </tr>
                         <tr>
-                          <td>Total</td>
-                          <td></td>
-                          <td></td>
-                          <td></td>
-                          <td></td>
-                          <td>{this.state.total}</td>
+                          <td colSpan="6"><button onClick={this.handleConfirm} id="confirmPurchaseButton" className="hoverEffect">Confirm</button></td>
                         </tr>
                     </tfoot>
                 </table>
-                <p>{JSON.stringify(this.state,null,2)}</p>
-                <p>{JSON.stringify(this.props,null,2)}</p>
             </div>
         )
     }
